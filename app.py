ad2392e8f698bbeabb022f5f1529e0a01fb00693
Pablo Sánchez <pabloastadeo@gmail.com>
2022-12-19 23:07:11 -0600

    Remove an unnecessary loop (#5)

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 from json import dump
 
 from spotify.spotify import get_spotify_complete_library
-from youtube_music.youtube import (
-    add_songs_to_library,
-    search_song,
-)
+from youtube_music.youtube import add_songs_to_library, search_song
 
 
 def spotify_to_youtube_music():
@@ -15,24 +12,26 @@ def spotify_to_youtube_music():
     print("\033[1;36mMigrating songs \033[0m")
     total_tracks = len(track_list)
     for i, track in enumerate(track_list):
-        song = search_song(
-            track["title"], track["artists"], track["album"], track["duration"], True
-        )
+        title = track["track"]["name"]
+        artists = [artist["name"] for artist in track["track"]["artists"]]
+        album = track["track"]["album"]["name"]
+        duration = round(track["track"]["duration_ms"] / 1000)
+
+        song = search_song(title, artists, album, duration, True)
 
         if not song:
-            song = search_song(
-                track["title"],
-                track["artists"],
-                track["album"],
-                track["duration"],
-                False,
-            )
+            song = search_song(title, artists, album, duration, False)
 
         if not song or not song.get("tokens"):
-            print(
-                f'\033[1;36m{track["title"]}, {track["artists"][0]}, {track["album"]} not found \033[0m'
+            print(f"\033[1;36m{title}, {artists[0]}, {album} not found \033[0m")
+            not_found.append(
+                dict(
+                    title=title,
+                    artists=artists,
+                    album=album,
+                    duration=duration,
+                )
             )
-            not_found.append(track)
             continue
 
         print(

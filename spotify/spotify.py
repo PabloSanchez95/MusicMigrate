a0d61702513136d3842ee4c03a968acac58984d2
Pablo Sanchez <pabloastadeo@gmail.com>
2022-12-19 23:02:28 -0600

    Remove an unnecessary loop

--- spotify/spotify.py
+++ spotify/spotify.py
@@ -18,21 +18,6 @@ def get_spotipy_client():
     )
 
 
-def filter_song_list(library_items: dict) -> list:
-    song_list = []
-    for song in library_items:
-        song_info = song["track"]
-        song_list.append(
-            dict(
-                title=song_info["name"],
-                artists=[artist["name"] for artist in song_info["artists"]],
-                album=song_info["album"]["name"],
-                duration=round(song_info["duration_ms"] / 1000),
-            )
-        )
-    return song_list
-
-
 def get_spotify_complete_library() -> list:
     print("\033[1;36mGetting Spotify library \033[0m")
     spotipy = get_spotipy_client()
@@ -46,7 +31,7 @@ def get_spotify_complete_library() -> list:
     while not offset or library_items:
         print(f"offset: {offset}")
         if library_items:
-            complete_library += filter_song_list(library_items)
+            complete_library += library_items
         library = spotipy.current_user_saved_tracks(limit=limit, offset=offset)
         library_items = library.get("items")
         offset += limit

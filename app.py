fc9bbbcdcf181f5d2a54d447d4a06d0b012f15c7
Pablo Sánchez <pabloastadeo@gmail.com>
2022-12-21 14:18:24 -0600

    Migrate spotify playlists to ytmusic (#6)

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
-from json import dump
+from json import dump, load
+from re import sub
+from time import sleep
 
-from spotify.spotify import get_spotify_complete_library
-from youtube_music.youtube import add_songs_to_library, search_song
+from spotify.spotify import (
+    get_spotify_complete_library,
+    get_spotify_playlist_tracks,
+    get_spotify_playlists,
+)
+from youtube_music.youtube import (
+    add_songs_to_library,
+    add_songs_to_playlist,
+    create_yt_playlist,
+    search_song,
+)
 
 
-def spotify_to_youtube_music():
-    track_list = list(reversed(get_spotify_complete_library()))
-    dump(track_list, open("spotify/tracklist.json", "w"))
-
+def migrate_songs(track_list: list, playlist_info: dict = None):
     not_found = []
     print("\033[1;36mMigrating songs \033[0m")
     total_tracks = len(track_list)
@@ -29,7 +37,7 @@ def spotify_to_youtube_music():
                     title=title,
                     artists=artists,
                     album=album,
-                    duration=duration,
+                    duration=track["track"]["duration_ms"],
                 )
             )
             continue
@@ -37,10 +45,83 @@ def spotify_to_youtube_music():
         print(
             f'{int((i+1) * 100 / total_tracks)}% - {song["title"]}, {song["artists"]}, {song["album"]}'
         )
-        add_songs_to_library(song.get("tokens", {}).get("add"))
 
-    dump(not_found, open("youtube_music/not_found.json", "w"))
+        try:
+            if playlist_info:
+                add_songs_to_playlist(playlist_info.get("id"), song.get("videoId"))
+            else:
+                add_songs_to_library(song.get("tokens", {}).get("add"))
+        except Exception as e:
+            print(str(e))
+            print(f"{title}, {artists} not added")
+            not_found.append(
+                dict(title=title, artists=artists, album=album, duration=duration)
+            )
+
+    if playlist_info:
+        not_found = dict(
+            playlistName=playlist_info["name"],
+            playlistId=playlist_info["id"],
+            tracks=not_found,
+        )
+        playlist_info["name"] = sub("[^A-Za-z0-9 ]", "", playlist_info["name"])
+        playlist_info["name"] = playlist_info["name"].lower().replace(" ", "_") + "_"
+
+    dump(
+        not_found,
+        open(f"youtube_music/{playlist_info.get('name') or ''}not_found.json", "w"),
+    )
+
+
+def spotify_to_youtube_music():
+    track_list = list(reversed(get_spotify_complete_library()))
+    dump(track_list, open("spotify/tracklist.json", "w"))
+
+    migrate_songs(track_list)
+
+
+def spotify_to_youtube_music_playlists():
+    spotify_playlists = list(reversed(get_spotify_playlists()))
+    dump(spotify_playlists, open("spotify/playlists.json", "w"))
+
+    i = 0
+    while i < len(spotify_playlists):
+        playlist = spotify_playlists[i]
+        playlist_name = playlist["name"]
+        playlist_description = playlist["description"]
+
+        try:
+            playlist_id = create_yt_playlist(playlist_name, playlist_description)
+        except Exception as e:
+            print(str(e))
+            sleep(5)
+            continue
+
+        tracks = get_spotify_playlist_tracks(playlist["id"], playlist_name)
+        migrate_songs(tracks, dict(name=playlist_name, id=playlist_id))
+        i += 1
+
+
+def retry_yt_not_found(file_name: str):
+    yt_not_found = load(open(file_name, "r"))
+    tracks = yt_not_found.get("tracks")
+
+    for i in range(len(tracks)):
+        tracks[i] = dict(
+            track=dict(
+                name=tracks[i]["title"],
+                artists=[{"name": artist_name} for artist_name in tracks[i]["artists"]],
+                album=dict(name=tracks[i]["album"]),
+                duration_ms=tracks[i]["duration"],
+            )
+        )
+
+    migrate_songs(
+        tracks,
+        dict(name=yt_not_found.get("playlistName"), id=yt_not_found.get("playlistId")),
+    )
 
 
 if __name__ == "__main__":
     spotify_to_youtube_music()
+    spotify_to_youtube_music_playlists()
--- spotify/spotify.py
+++ spotify/spotify.py
@@ -9,11 +9,15 @@ def get_spotipy_client():
     load_dotenv()
     client_id = os.getenv("SPOTIPY_CLIENT_ID")
     client_secret = os.getenv("SPOTIPY_CLIENT_SECRET")
-    scope = "user-library-read"
 
+    scopes = [
+        "playlist-read-private",
+        "user-library-read",
+        "playlist-read-collaborative",
+    ]
     return spotipy.Spotify(
         auth_manager=SpotifyOAuth(
-            client_id=client_id, client_secret=client_secret, scope=scope
+            client_id=client_id, client_secret=client_secret, scope=scopes
         )
     )
 
@@ -29,11 +33,55 @@ def get_spotify_complete_library() -> list:
     complete_library = []
 
     while not offset or library_items:
-        print(f"offset: {offset}")
         if library_items:
             complete_library += library_items
+
         library = spotipy.current_user_saved_tracks(limit=limit, offset=offset)
         library_items = library.get("items")
         offset += limit
 
     return complete_library
+
+
+def get_spotify_playlists():
+    print("\033[1;36mGetting Spotify playlists \033[0m")
+    spotipy = get_spotipy_client()
+
+    offset = 0
+    limit = 50
+    curr_playlist_offset = []
+
+    all_playlists = []
+
+    while not offset or curr_playlist_offset:
+        if curr_playlist_offset:
+            all_playlists += curr_playlist_offset
+
+        playlist_response = spotipy.current_user_playlists(limit=limit, offset=offset)
+        curr_playlist_offset = playlist_response.get("items")
+        offset += limit
+
+    return all_playlists
+
+
+def get_spotify_playlist_tracks(playlist_id: str, name: str):
+    print(f"\033[1;36mGetting {name} tracks \033[0m")
+    spotipy = get_spotipy_client()
+
+    offset = 0
+    limit = 50
+    curr_track_offset = []
+
+    all_tracks = []
+
+    while not offset or curr_track_offset:
+        if curr_track_offset:
+            all_tracks += curr_track_offset
+
+        playlist_tracks = spotipy.playlist_items(
+            playlist_id=playlist_id, limit=limit, offset=offset
+        )
+        curr_track_offset = playlist_tracks.get("items")
+        offset += limit
+
+    return all_tracks
